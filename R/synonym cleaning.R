e3b578f63efa90f283c5d791ece485281a8ac69d
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2022-03-01 12:28:14 +1100

    Update synonym cleaning.R

--- R/synonym cleaning.R
+++ R/synonym cleaning.R
@@ -1,6 +1,5 @@
-#extract 3 columns from the original APC file - nameType, acceptedNameUsage, taxonomicStatus, scientificName, canonicalName and taxonRank
+#extract columns from the original APC file - nameType, acceptedNameUsage, taxonomicStatus, scientificName, canonicalName and taxonRank
 
-#reorder columns so that the scientificName column is first. This is the column from which the potential synonyms on iNat are matched
 
 #within taxonRank, filter to only include, species, forma, varietas, subspecies
 
@@ -10,7 +9,5 @@
 
 apc <- read_csv("data/APC-taxon-2022-02-14-5132.csv")
 step1_apc<-dplyr::select(apc, nameType, acceptedNameUsage, taxonomicStatus, scientificName, canonicalName, taxonRank)
-
-step2_apc <- step1_apc[, c(1,6,4,3,2,5)]
 
 step3_apc <- filter(step2_apc, nameType=="scientific")
