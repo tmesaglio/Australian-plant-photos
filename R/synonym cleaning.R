218d8c260c68231ca90a5bd2c9a98376acb42c1c
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2022-03-01 14:37:50 +1100

    Update synonym cleaning.R

--- R/synonym cleaning.R
+++ R/synonym cleaning.R
@@ -1,13 +1,9 @@
-#extract columns from the original APC file - nameType, acceptedNameUsage, taxonomicStatus, scientificName, canonicalName and taxonRank
+library(tidyverse)
+library(stringr)
 
 
-#within taxonRank, filter to only include, species, forma, varietas, subspecies
-
-
-
-#let's try it out
 
 apc <- read_csv("data/APC-taxon-2022-02-14-5132.csv")
 step1_apc<-dplyr::select(apc, nameType, acceptedNameUsage, taxonomicStatus, scientificName, canonicalName, taxonRank)
 
-step3_apc <- filter(step2_apc, nameType=="scientific")
+step2_apc <- filter(step1_apc, nameType=="scientific")
