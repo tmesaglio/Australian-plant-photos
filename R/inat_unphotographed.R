efd6880977c100ed6da2429cbd3d49dcffcffd11
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2022-03-23 17:30:30 +1100

    creating unphotographed file, plus minor cleaning of other files

---
+++ R/inat_unphotographed.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(stringr)
+library(dplyr)
+
+#read files
+final_matrix <- read_csv("data/final_apc_matrix.csv")
+iM5 <- read_csv("data/iM5.csv")
+
+#find matches between iM5 and final_matrix
+new_final_matrix <- final_matrix %>% mutate(Match = case_when(canonicalName %in% iM5$APC_name ~canonicalName, T ~ "No")) 
+unphotographed_inat <- filter(new_final_matrix, Match=="No")
+
+
+
