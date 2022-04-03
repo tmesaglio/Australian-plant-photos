6bc304f2b25be121330dc8e99fb86dfe49298018
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2022-04-04 00:23:00 +1000

    Update rainforest.R

--- R/rainforest.R
+++ R/rainforest.R
@@ -6,3 +6,11 @@ library(dplyr)
 pre_rainforest <- read_csv("data/rainforest.csv")
 
 pre_rainforest2<-pre_rainforest[!grepl("aceae", pre_rainforest$species),]
+
+#match to unphotographed
+
+unphoto <- read_csv("data/unphotographed_inat_ala_florabase_euclid_wattle.csv")
+
+pre_rainforest3 <- pre_rainforest2 %>% mutate(Match = case_when(species %in% unphoto$canonicalName ~ "Yes", T ~ "No"))
+
+pre_rainforest4 <- dplyr::filter(pre_rainforest3, Match == "Yes")
