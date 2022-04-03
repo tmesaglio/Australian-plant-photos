7cbfdd82f9f58e3a3774fd741cd4c945067fa5c0
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2022-04-03 20:44:12 +1000

    euclid and wattle

--- R/florabase_euclid_wattle.R
+++ R/florabase_euclid_wattle.R
@@ -13,3 +13,13 @@ unphotographed_inat_ala_florabase <- filter(unphotographed_inat_ala_florabase, M
 #via the ALA query, which I had been doing simultaneously 
 
 write_csv(unphotographed_inat_ala_florabase,"data/unphotographed_inat_ala_florabase.csv")
+
+
+#as above for eucalypts and wattles
+euclid_wattle <- read_csv("data/euclid_wattle.csv")
+unphotographed_inat_ala_florabase <- read_csv("data/unphotographed_inat_ala_florabase.csv")
+
+unphotographed_inat_ala_florabase_euclid_wattle <- unphotographed_inat_ala_florabase %>% mutate(Match4 = case_when(canonicalName %in% euclid_wattle$species ~ "Yes", T ~ "No")) 
+unphotographed_inat_ala_florabase_euclid_wattle <- filter(unphotographed_inat_ala_florabase_euclid_wattle, Match4=="No")
+
+write_csv(unphotographed_inat_ala_florabase_euclid_wattle,"data/unphotographed_inat_ala_florabase_euclid_wattle.csv")
