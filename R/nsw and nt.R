ce3264dc8e380d62c0977278aa21964125308fbb
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2022-04-07 14:03:00 +1000

    Update nsw and nt.R

--- R/nsw and nt.R
+++ R/nsw and nt.R
@@ -2,8 +2,12 @@ library(tidyverse)
 library(stringr)
 library(dplyr)
 
-
+#NSW query (plantNET)
 unphotographed <- read_csv("data/unphotographed_all_tas_vic_pea.csv")
 
-plantnet <-read_csv("data/PlantNET_raw.csv")
+plantnet <-read_csv("data/PlantNET.csv")
+plantnet_matched<-filter(plantnet, photo=="yes")
+
+unphotographed_plantnet <- unphotographed %>% mutate(Match9 = case_when(canonicalName %in% plantnet_matched$canonical_name ~ "Yes", T ~ "No")) 
+unphotographed2 <- filter(unphotographed_plantnet, Match9=="No")
 
