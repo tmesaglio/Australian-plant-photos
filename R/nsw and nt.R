95586773c554b65e836c6e66b27302deaabd9439
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2022-04-07 15:49:30 +1000

    nsw queries

--- R/nsw and nt.R
+++ R/nsw and nt.R
@@ -11,3 +11,20 @@ plantnet_matched<-filter(plantnet, photo=="yes")
 unphotographed_plantnet <- unphotographed %>% mutate(Match9 = case_when(canonicalName %in% plantnet_matched$canonical_name ~ "Yes", T ~ "No")) 
 unphotographed2 <- filter(unphotographed_plantnet, Match9=="No")
 
+#nsw lucid key
+
+se_nsw <- read_csv("data/se_nsw.csv")
+se_nsw_match <- se_nsw %>% mutate(Match = case_when(species %in% unphotographed2$canonicalName ~ "Yes", T ~ "No")) 
+se_nsw_match_yes<- filter(se_nsw_match, Match =="Yes")
+
+write_csv(se_nsw_match_yes,"data/se_nsw_match_yes.csv")
+
+updated_se_nsw <- read_csv("data/se_nsw_match_yes.csv")
+
+matched <- dplyr::filter(updated_se_nsw, photo == "yes")
+
+unphotographed_plantnet_lucid <- unphotographed2 %>% mutate(Match10 = case_when(canonicalName %in% matched$species ~ "Yes", T ~ "No")) 
+unphotographed3 <- filter(unphotographed_plantnet_lucid, Match10=="No")
+
+#NT
+
