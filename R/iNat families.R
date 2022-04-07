225104aa597b5ac0fd9bdd21d3b966098f32bbed
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2022-04-08 00:03:27 +1000

    Update iNat families.R

--- R/iNat families.R
+++ R/iNat families.R
@@ -59,3 +59,19 @@ grass<-filter(inat_families, family=="Poaceae")
 grass2<-dplyr::select(grass, APC_name, family)
 
 write_csv(grass2,"data/grass.csv")
+
+
+#update after ausgrass done
+apc <- read_csv("data/APC-taxon-2022-02-14-5132.csv")
+unphotographed <- read_csv("data/unphotographed_all_grass.csv")
+
+apc2 <- filter(apc, taxonRank == "Species",taxonomicStatus=="accepted",nameType=="scientific")
+families<-dplyr::select(apc2, canonicalName, family)
+
+families<-rename(families, APC_name = canonicalName)
+unphotographed<-rename(unphotographed, APC_name = canonicalName)
+
+inat_families<- dplyr::inner_join(unphotographed, families, by = "APC_name")
+
+library(epiDisplay)
+tab1(inat_families$family, sort.group = "decreasing")
