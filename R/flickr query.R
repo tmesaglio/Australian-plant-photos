0f775d87d40af3e73d210df762ebe97bad2838d1
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2022-04-11 22:27:12 +1000

    flickr update

--- R/flickr query.R
+++ R/flickr query.R
@@ -6,3 +6,23 @@ library(devtools)
 unphotographed <- read_csv("data/unphotographed_all_apii.csv")
 
 devtools::install_github("nfox29/photosearcher")
+
+library(photosearcher)
+
+#testing the flickr api
+rock_climbing <- photo_search(
+  mindate_taken = "2010-01-01",
+  maxdate_taken = "2018-01-01",
+  text = "rock climbing",
+  bbox = "-12.875977,49.210420,2.636719,59.977005",
+  has_geo = TRUE
+) 
+
+
+#that worked, so now test for a plant species
+
+diuris <- photo_search(mindate_taken = "1960-01-01",
+                              maxdate_taken = "2022-04-11",
+                              text = "Diuris",
+                              has_geo = FALSE)
+                              
