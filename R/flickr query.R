184eab7fbc24c9d69e418e6273f8d40ecb97e419
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2022-04-12 11:35:54 +1000

    Update flickr query.R

--- R/flickr query.R
+++ R/flickr query.R
@@ -9,20 +9,17 @@ devtools::install_github("nfox29/photosearcher")
 
 library(photosearcher)
 
-#testing the flickr api
-rock_climbing <- photo_search(
-  mindate_taken = "2010-01-01",
-  maxdate_taken = "2018-01-01",
-  text = "rock climbing",
-  bbox = "-12.875977,49.210420,2.636719,59.977005",
-  has_geo = TRUE
-) 
+diuris <- photo_search(mindate_taken = "1960-01-01", 
+                       maxdate_taken = "2022-04-11", 
+                       text = "Diuris", 
+                       has_geo = FALSE)
 
-
-#that worked, so now test for a plant species
-
-diuris <- photo_search(mindate_taken = "1960-01-01",
-                              maxdate_taken = "2022-04-11",
-                              text = "Diuris brevissima",
-                              has_geo = FALSE)
-                              
+Diuris_brockmanii<-dplyr::filter(diuris,grepl("Diuris brockmanii",diuris$title,ignore.case = T)|
+                                   grepl("Diuris brockmanii",diuris$tags,ignore.case = T)|
+                                   grepl("Diuris brockmanii",diuris$description,ignore.case = T)|
+                                   grepl("Diurisbrockmanii",diuris$title,ignore.case = T)|
+                                   grepl("Diurisbrockmanii",diuris$tags,ignore.case = T)|
+                                   grepl("Diurisbrockmanii",diuris$description,ignore.case = T)|
+                                   grepl("Diuris_brockmanii",diuris$title,ignore.case = T)|
+                                   grepl("Diuris_brockmanii",diuris$tags,ignore.case = T)|
+                                   grepl("Diuris_brockmanii",diuris$description,ignore.case = T))
