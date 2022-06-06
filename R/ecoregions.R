98abeacb36691a65eff6f0188a877836c3852c8e
Thomas Mesaglio <thomasmesaglio@hotmail.com>
2022-06-06 16:58:10 +1000

    Update ecoregions.R

--- R/ecoregions.R
+++ R/ecoregions.R
@@ -19,20 +19,18 @@ xx <- point.in.poly(coords2, biomes, sp = TRUE, duplicate = TRUE)
 # back to dataframe and save
 coords3 <- as.data.frame(xx)
 
-write_csv(coords3, "data/ecoregions.csv")
+#remove all rows with NAs (ALA records that fall into the ocean due to entry error, GPS misreadings, etc)
 
-#note that in the ~200,000 row csv produced here, ~9600 rows have NA values for ecoregion. These are records where either the coordinates
-#are close to the edge of land/sea or slightly in the ocean; the shape file I used is terrestrial only, and clearly seems to not have 
-#ultra high resolution around the boundaries, so these values get missed out. I filled them in manually outside R (filename = 'ecoregions_updated')
+coords4 <- coords3 %>% drop_na(ECO_NAME)
 
-#also note: whilst I could have filled in many/most of these NAs programatically in R based on neighbour values in the df, there are also
-#a number of NAs right on the boundary of two ecoregion, with cases where rows keep swapping back and forth between them, so I was more 
-#comfortable doing it manually for accuracy.
+coords5 <- coords4 %>%
+  group_by(APC_name, ECO_NAME) %>%
+  summarise(number = n()) %>%
+  arrange(desc(number))
 
-#third note: I removed a number of rows from the ecoregion csv due to them being wildly erroneous points in the middle of the ocean
-#(the points that I had previously cleaned manually in an earlier figure, as noted above in lines 391-392)
+coords6<-dplyr::arrange(coords5, APC_name)
 
-#fourth note: for some rows, I added my own new 'ecoregion': "Offshore sand and reef flats and islands". This is to address records from 
-#Ashmore Reef, Coral Sea Islands, etc., which for some reason have no designated ecoregion under the current global classification.
+coords7<- coords6 %>% group_by(APC_name) %>% summarise(Value = ECO_NAME[which.max(number)],num.obs=max(number))
 
-#I SCRAPPED THIS
+
+
